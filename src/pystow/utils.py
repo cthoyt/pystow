1d6a2f67afbbe9748f15fb6e308ee7c78476a262
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-27 23:39:55 +0100

    Add get commit function

--- src/pystow/utils.py
+++ src/pystow/utils.py
@@ -10,6 +10,7 @@ import tarfile
 import zipfile
 from io import BytesIO, StringIO
 from pathlib import Path, PurePosixPath
+from subprocess import check_output  # noqa: S404
 from typing import Union
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
@@ -148,3 +149,18 @@ def read_tarfile_csv(path: Union[str, Path], inner_path: str, sep='\t', **kwargs
     with tarfile.open(path) as tar_file:
         with tar_file.extractfile(inner_path) as file:  # type: ignore
             return pd.read_csv(file, sep=sep, **kwargs)
+
+
+def get_commit(org: str, repo: str, provider: str = 'git') -> str:
+    """Get last commit hash for the given repo."""
+    if provider == 'git':
+        r = check_output(['git', 'ls-remote', f'https://github.com/{org}/{repo}'])  # noqa
+        r = (line.strip().split('\t') for line in r.decode('utf8').splitlines())
+        rv = next(line[0] for line in r if line[1] == 'HEAD')
+    elif provider == 'github':
+        res = requests.get(f'https://api.github.com/repos/{org}/{repo}/branches/master')
+        res_json = res.json()
+        rv = res_json['commit']['sha']
+    else:
+        raise NotImplementedError(f'invalid implementation: {provider}')
+    return rv
