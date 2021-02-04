55790495032b9e9cc3a086cf989e1f0bd2cd3a82
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-04 14:32:13 +0100

    Update __init__.py

--- src/pystow/__init__.py
+++ src/pystow/__init__.py
@@ -2,6 +2,6 @@
 
 """PyStow: Easily pick a place to store data for your python package."""
 
-from .api import ensure, ensure_csv, ensure_excel, ensure_tar_df, ensure_zip_df, get, module  # noqa
+from .api import ensure, ensure_csv, ensure_excel, ensure_rdf, ensure_tar_df, ensure_zip_df, get, module  # noqa
 from .conf import get_config  # noqa
 from .module import Module  # noqa
