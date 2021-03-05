3bbaff68a2c1ae8c844d76fd0e4b3dd38ae8ae5b
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-05 14:21:42 +0100

    Update CLI

--- src/pystow/cli.py
+++ src/pystow/cli.py
@@ -3,19 +3,41 @@
 """Command line interface for PyStow."""
 
 import os
+from typing import Optional, Sequence
 
 import click
 
-from .api import get
+from . import api
 
 
-@click.command()
+@click.group()
+def main():
+    """Run the PyStow CLI."""
+
+
+def _ls(directory):
+    command = f'ls -al {directory}'
+    click.secho(f'[pystow] {command}', fg='cyan', bold=True)
+    os.system(command)  # noqa:S605
+
+
+@main.command()
 @click.argument('keys', nargs=-1)
-def main(keys: str):
-    """List a PyStow directory."""
-    directory = get(*keys)
-    click.secho(f'[pystow] {directory}', fg='cyan', bold=True)
-    os.system(f'ls -al {directory}')  # noqa:S605
+def join(keys: Sequence[str]):
+    """List a directory."""
+    directory = api.join(*keys)
+    _ls(directory)
+
+
+@main.command()
+@click.argument('keys', nargs=-1)
+@click.option('--url', required=True)
+@click.option('--name')
+@click.option('--force', is_flag=True)
+def ensure(keys: Sequence[str], url: str, name: Optional[str], force: bool):
+    """Ensure a file is downloaded."""
+    path = api.ensure(*keys, url=url, name=name, force=force)
+    _ls(path.parent)
 
 
 if __name__ == '__main__':
