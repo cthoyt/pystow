57586f4c9bd15a010aed4b99ec6edb8ecc75c7ff
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-06 16:12:19 -0400

    Update CLI

--- src/pystow/cli.py
+++ src/pystow/cli.py
@@ -15,15 +15,17 @@ def main():
     """Run the PyStow CLI."""
 
 
-def _ls(directory):
-    command = f'ls -al {directory}'
-    click.secho(f'[pystow] {command}', fg='cyan', bold=True)
-    os.system(command)  # noqa:S605
+@main.command()
+@click.argument('keys', nargs=-1)
+@click.option('--name')
+def join(keys: Sequence[str], name: Optional[str]):
+    """List a directory."""
+    click.echo(api.join(*keys, name=name))
 
 
 @main.command()
 @click.argument('keys', nargs=-1)
-def join(keys: Sequence[str]):
+def ls(keys: Sequence[str]):
     """List a directory."""
     directory = api.join(*keys)
     _ls(directory)
@@ -38,6 +40,12 @@ def ensure(keys: Sequence[str], url: str, name: Optional[str], force: bool):
     """Ensure a file is downloaded."""
     path = api.ensure(*keys, url=url, name=name, force=force)
     _ls(path.parent)
+
+
+def _ls(directory):
+    command = f'ls -al {directory}'
+    click.secho(f'[pystow] {command}', fg='cyan', bold=True)
+    os.system(command)  # noqa:S605
 
 
 if __name__ == '__main__':
