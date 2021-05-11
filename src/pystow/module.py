93be7941831feace4275eb644e6548b494d3b502
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-11 11:12:42 -0400

    Automatically make README in data directory
    Closes #5

--- src/pystow/module.py
+++ src/pystow/module.py
@@ -9,6 +9,7 @@ import pickle
 import warnings
 from contextlib import contextmanager
 from pathlib import Path
+from textwrap import dedent
 from typing import Any, Mapping, Optional, Sequence, TYPE_CHECKING, Union
 
 from .utils import (
@@ -61,6 +62,62 @@ def get_base(key: str, ensure_exists: bool = True) -> Path:
 def _assert_valid(key: str) -> None:
     if '.' in key:
         raise ValueError(f'The module should not have a dot in it: {key}')
+
+
+README_TEXT = dedent('''\
+# PyStow Data Directory
+
+This directory is used by [`pystow`](https://github.com/cthoyt/pystow) as a
+reproducible location to store and access data.
+
+### ⚙️️ Configuration
+
+By default, data is stored in the `$HOME/.data` directory. By default, the `<app>` app will create the
+`$HOME/.data/<app>` folder.
+
+If you want to use an alternate folder name to `.data` inside the home directory, you can set the `PYSTOW_NAME`
+environment variable. For example, if you set `PYSTOW_NAME=mydata`, then the following code for the `pykeen` app will
+create the `$HOME/mydata/pykeen/` directory:
+
+```python
+import os
+import pystow
+
+# Only for demonstration purposes. You should set environment
+# variables either with your .bashrc or in the command line REPL.
+os.environ['PYSTOW_NAME'] = 'mydata'
+
+# Get a directory (as a pathlib.Path) for ~/mydata/pykeen
+pykeen_directory = pystow.join('pykeen')
+```
+
+If you want to specify a completely custom directory that isn't relative to your home directory, you can set
+the `PYSTOW_HOME` environment variable. For example, if you set `PYSTOW_HOME=/usr/local/`, then the following code for
+the `pykeen` app will create the `/usr/local/pykeen/` directory:
+
+```python
+import os
+import pystow
+
+# Only for demonstration purposes. You should set environment
+# variables either with your .bashrc or in the command line REPL.
+os.environ['PYSTOW_HOME'] = '/usr/local/'
+
+# Get a directory (as a pathlib.Path) for /usr/local/pykeen
+pykeen_directory = pystow.join('pykeen')
+```
+
+Note: if you set `PYSTOW_HOME`, then `PYSTOW_NAME` is disregarded.
+''')
+
+
+def ensure_readme():
+    """Ensure there's a README in the PyStow data directory."""
+    readme_path = get_home(ensure_exists=True).joinpath('README.md')
+    if readme_path.is_file():
+        return
+    with readme_path.open('w') as file:
+        print(README_TEXT, file=file)
 
 
 class Module:
